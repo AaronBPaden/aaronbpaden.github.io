488bc75e2a2241568a370bf2fb5a7b0e5cf6e679
Aaron Paden <aaronbpaden@gmail.com>
2021-09-24 14:30:28 -0500

    Fix expander transition.

--- js/mca-bio2.js
+++ js/mca-bio2.js
@@ -1,13 +1,19 @@
 'use strict'
+
+// On mobile, expand the about-text when pressing the arrow button.
 document.querySelector("#aboutButton").addEventListener("click", () => {
     let aboutBox = document.querySelector("#aboutBox");
     let aboutButton = document.querySelector("#aboutButton");
     let aboutText = document.querySelector("#aboutText");
-    aboutText.style.display = getComputedStyle(aboutText).display === "none" ? "block" : "none";
+    aboutText.style.visibility = getComputedStyle(aboutText).visibility === "hidden" ? "visible" : "hidden";
     aboutButton.textContent = aboutButton.textContent === "↑" ? "↓" : "↑";
+
+    // CSS transitions require a static height, so we need to calculate it.
+    let expandedHeight = aboutText.clientHeight + aboutBox.clientHeight;
+
     // It seems the style property only keeps track of inline styles, so we need to check for an
     // empty style as well.
-    aboutBox.style.height = aboutBox.style.height === "" ? "100%"
-        : aboutBox.style.height === "15vh" ? "100%"
+    aboutBox.style.height = aboutBox.style.height === "" ? expandedHeight + "px"
+        : aboutBox.style.height === "15vh" ? expandedHeight + "px"
         : "15vh";
 });
