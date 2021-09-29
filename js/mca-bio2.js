8d00cd132fec1a7d139e415aa4d91a306703d8f2
Aaron Paden <aaronbpaden@gmail.com>
2021-09-29 11:18:16 -0500

    Implement certs page.

--- js/mca-bio2.js
+++ js/mca-bio2.js
@@ -1,16 +1,27 @@
 'use strict'
 
 // On mobile, expand the about-text when pressing the arrow button.
-document.querySelector("#aboutButton").addEventListener("click", () => {
-    let aboutBox = document.querySelector("#aboutBox");
-    let aboutButton = document.querySelector("#aboutButton");
-    let aboutText = document.querySelector("#aboutText");
-    aboutText.style.visibility = getComputedStyle(aboutText).visibility === "hidden" ? "visible" : "hidden";
-    aboutButton.textContent = aboutButton.textContent === "↑" ? "↓" : "↑";
+let aboutButton = document.querySelector("#aboutButton");
+if (aboutButton) {
+    document.querySelector("#aboutButton").addEventListener("click", () => {
+        let aboutBox = document.querySelector("#aboutBox");
+        let aboutButton = document.querySelector("#aboutButton");
+        let aboutText = document.querySelector("#aboutText");
+        aboutText.style.visibility = getComputedStyle(aboutText).visibility === "hidden" ? "visible" : "hidden";
+        aboutButton.textContent = aboutButton.textContent === "↑" ? "↓" : "↑";
 
-    // It seems the style property only keeps track of inline styles, so we need to check for an
-    // empty style as well.
-    aboutBox.style.height = aboutBox.style.height === "" ? aboutBox.scrollHeight + "px"
-        : aboutBox.style.height === "15vh" ? aboutBox.scrollHeight + "px"
-        : "15vh";
-});
+        // It seems the style property only keeps track of inline styles, so we need to check for an
+        // empty style as well.
+        aboutBox.style.height = aboutBox.style.height === "" ? aboutBox.scrollHeight + "px"
+            : aboutBox.style.height === "15vh" ? aboutBox.scrollHeight + "px"
+            : "15vh";
+    });
+}
+
+if (document.querySelector('#certCarousel')) {
+    // AFAICT, slick requires you to use JQuery here.
+    $('#certCarousel').slick({
+        prevArrow: '<button class="slick-prev btn" id="prevArrow">←</button>',
+        nextArrow: '<button class="slick-next btn" id="nextArrow">→</button>'
+    });
+}
