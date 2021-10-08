b7dbc178de862df72d249cda12b844e2c916fa81
Aaron Paden <aaronbpaden@gmail.com>
2021-10-08 11:19:05 -0500

    Switch to case here because it's more readable.

--- js/mca-bio2.js
+++ js/mca-bio2.js
@@ -12,9 +12,17 @@ if (aboutButton) {
 
         // It seems the style property only keeps track of inline styles, so we need to check for an
         // empty style as well.
-        aboutBox.style.height = aboutBox.style.height === "" ? aboutBox.scrollHeight + "px"
-            : aboutBox.style.height === "15vh" ? aboutBox.scrollHeight + "px"
-            : "15vh";
+        switch (aboutBox.style.height) {
+            case "":
+            case "15vh":
+                aboutBox.style.height = aboutBox.scrollHeight + "px";
+                break;
+            default:
+                aboutBox.style.height = "15vh";
+        }
+        // aboutBox.style.height = aboutBox.style.height === "" ? aboutBox.scrollHeight + "px"
+        //     : aboutBox.style.height === "15vh" ? aboutBox.scrollHeight + "px"
+        //     : "15vh";
     });
 }
 
